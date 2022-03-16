48e91bf611994e88cd0549b567d87b80713b575a
Suyoun <sykim51774@gmail.com>
2022-03-16 17:05:12 +0900

    Award Router 만들기

---
+++ back/src/routers/awardRouter.js
@@ -0,0 +1,10 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { awardService } from "../services/awardService";
+
+const awardRouter = Router();
+
+awardRouter.post("/user/login", async (req, res, next) => {
+
+});
