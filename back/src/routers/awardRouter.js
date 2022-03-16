949a4c7ae5067a7ae9bb2469feee7124abd73ef4
Suyoun <sykim51774@gmail.com>
2022-03-16 17:39:19 +0900

    Award Router 수상이력 생성 데이터 가져오기

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -1,10 +1,14 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { awardService } from "../services/awardService";
+import { awardService } from "../services/awardService.js";
 
 const awardRouter = Router();
 
-awardRouter.post("/user/login", async (req, res, next) => {
+awardRouter.post("/award/create", async (req, res, next) => {
+    // req에서 데이터 가져오기
+    const title = req.body.title;
+    const description = req.body.description;
+    const whenDate = req.body.whenDate;
 
 });
