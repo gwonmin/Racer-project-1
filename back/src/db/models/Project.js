56db40d3eae748f9da178cf39a127655664e3071
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-16 16:23:54 +0900

    create/update/findAll 구현

--- back/src/db/models/Project.js
+++ back/src/db/models/Project.js
@@ -1,9 +1,23 @@
 import ProjectModel from "../schemas/project"
 
 class Project{
-    static findAll = async() =>{
+    static create = async ({ newProject }) =>{
+        const createdNewProject  =  await ProjectModel.create(newProject);
+        return createdNewProject;
+    }
+
+    static findAll = async () =>{
         const projects = await ProjectModel.find({});
         return projects;
+    }
+
+    static update = async ({ project_id, }) => {
+        const filter = { id: project_id };
+        const update = { }
+        const updatedProject = await ProjectModel.findOneAndUpdate(
+            filter,
+
+        )
     }
 }
 
