7798b5f576fc7842a07e7eb432e23a0814719b23
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-17 17:12:15 +0900

    프로젝트 create할 때 사용할 addProject 추가

--- back/src/services/projectService.js
+++ back/src/services/projectService.js
@@ -1,6 +1,18 @@
 import Project from "../db";
+import { v4 as uuidv4 } from "uuid";
 
 class projectService{
+    static addProject = async ({ title, from_date, to_date })=>{
+        const id = uuidv4();
+        const newProject = { id, title, from_date, to_date };
+
+        const createdNewProject = await Project.create({ newProject });
+        createdNewProject.errorMessage = Null;
+
+        return createdNewProject;
+
+    }
+
     static getProjects = async () =>{
         const projects = await Project.findAll();
         return projects;
