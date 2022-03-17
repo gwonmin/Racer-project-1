9f3094fe62c156539b1bb12b75338cefa4050d3a
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-17 17:11:01 +0900

    프로젝트 create 구현

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -1,15 +1,28 @@
 import { Router } from "express";
+import { is } from "express/lib/request";
 import { login_required } from "../middlewares/login_required";
 import { projectService } from "../services/projectService";
 
 const projectRouter = Router();
 
-projectRouter.get(
-    "/projects", 
-    login_required,
-    async (req, res, next)=>{
+projectRouter.post("/create", async (req, res, next)=>{
+    try{
+        if(is.emptyObject(req.body)) {
+            throw new Error(
+                "headers의 Content-Type를 application/json으로 설정해주세요"
+            );
+        }
+
+        console.log(req.body);
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 전체 프로젝트 목록을 얻음
+projectRouter.get("/", login_required, async (req, res, next)=>{
         try {
-            // 전체 프로젝트 목록을 얻음
             const projects = await projectService.getProjects();
             res.status(200).send(projects)
         } catch (err){
@@ -17,5 +30,10 @@ projectRouter.get(
         }
     }
 );
+
+projectRouter.get("/:id", async (req, res, next)=>{
+
+    }
+)
 
 export default { projectRouter }
