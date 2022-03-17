166ac1231e86fd42a1cff8531d20bac02a19500e
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-17 17:28:38 +0900

    project 필드 user_id 추가

--- back/src/db/schemas/project.js
+++ back/src/db/schemas/project.js
@@ -6,6 +6,10 @@ const ProjectSchema = new Schema(
             type: String,
             required: true,
         },
+        user_id:{
+          type: String,
+          required: true,
+        },
         title:{
             type: String,
             required: true,
