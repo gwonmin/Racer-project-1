60cc8bc8c42260afd60793880228944fa44cd32e
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-03-18 05:17:44 +0000

    Update index.js merge conflict 해결

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
-import { Certificate } from "./models/Certificate";
 import { Education } from "./models/Education";
 
 const DB_URL =
@@ -17,4 +16,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Certificate, Education };
+export { User, Education };
