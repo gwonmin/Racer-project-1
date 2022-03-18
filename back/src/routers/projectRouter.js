52335d6574debe55642df95e3e5a1c92f0243bd7
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-18 12:38:38 +0900

    project create 완성/ project update 완성

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -6,7 +6,7 @@ import { projectService } from "../services/projectService";
 const projectRouter = Router();
 
 // 새로운 프로젝트 만들기
-projectRouter.post("/create", async (req, res, next)=>{
+projectRouter.post("/project/create", async (req, res, next)=>{
     try{
         if(is.emptyObject(req.body)) {
             throw new Error(
@@ -37,19 +37,49 @@ projectRouter.post("/create", async (req, res, next)=>{
 });
 
 // 전체 프로젝트 목록을 얻음
-projectRouter.get("/", login_required, async (req, res, next)=>{
+projectRouter.get(
+    "/projects", 
+    login_required, 
+    async (req, res, next)=>{
         try {
             const projects = await projectService.getProjects();
             res.status(200).send(projects)
-        } catch (err){
-            next(err)
+        } catch (error){
+            next(error)
         }
     }
 );
 
-projectRouter.get("/:id", async (req, res, next)=>{
+// 프로젝트 수정-업데이트
+projectRouter.put(
+    "/projects/:id",
+    login_required,
+    async function (req, res, next) {
+      try {
+        // URI로부터 사용자 id를 추출함.
+        const user_id = req.params.id;
 
+        // body data 로부터 업데이트할 프로젝트 정보를 추출함.
+        const title = req.body.title ?? null;
+        const from_date = req.body.from_date ?? null;
+        const to_date = req.body.pto_date?? null;
+        const description = req.body.description ?? null;
+        const git = req.body.git ?? null;
+
+        const toUpdate = { title, from_date, to_date, description, git };
+  
+        // 해당 사용자 아이디로 프로젝트 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+        const updatedProject = await projectService.setProject({ user_id, toUpdate });
+  
+        if (updatedProject.errorMessage) {
+          throw new Error(updatedProject.errorMessage);
+        }
+  
+        res.status(200).json(updatedProject);
+      } catch (error) {
+        next(error);
+      }
     }
-)
+  );
 
 export default { projectRouter }
