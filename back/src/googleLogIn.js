c1ce457e7c486b7428ba36c2f4a2dac211b8e85a
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-23 16:37:40 +0900

    Serverside LogIn -https://developers.google.com/identity/sign-in/web/backend-auth 참고

--- back/src/googleLogIn.js
+++ back/src/googleLogIn.js
@@ -1,17 +1,21 @@
 const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
-async function verify() {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
+const verify = async () => {
+    try{
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+    } catch(error){
+        console.log(error);
+    };
+  
 }
 
-verify().catch(console.error);
