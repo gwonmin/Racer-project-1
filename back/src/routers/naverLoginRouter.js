8f3839336d3f9c4a665e1a0a2b5cabd639691b60
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-03-23 20:37:21 +0900

    회원 프로필 조회 api 기능 추가 및 오류 수정

--- back/src/routers/naverLoginRouter.js
+++ back/src/routers/naverLoginRouter.js
@@ -3,7 +3,6 @@ import { Router } from "express";
 
 const naverLoginRouter = Router();
 
-
 var client_id = '3wO8fO549CGAeAT0mcNu';
 var client_secret = process.env.naverLoginClientSecret;
 
@@ -12,12 +11,18 @@ var state = "RANDOM_STATE";
 var redirectURI = encodeURI("http://127.0.0.1:5001/callback");
 var api_url = "";
 
+var token = process.env.naverLoginToken;
+var refreshToken = process.env.naverLoginRefreshToken;
+var header = "Bearer " + token; // Bearer 다음에 공백 추가
+
+//네이버 로그인 api
 naverLoginRouter.get('/naverlogin', function (req, res) {
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
    res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
  });
  
+ //로그인 토큰정보를 주는 api
 naverLoginRouter.get('/callback', function (req, res) {
     code = req.query.code;
     state = req.query.state;
@@ -30,13 +35,47 @@ naverLoginRouter.get('/callback', function (req, res) {
      };
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        res.end(body);
+        // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        // body는 객체타입이 아니라 String이기 때문에 사용하기 쉽도록 객체타입으로 형변환
+
+        let objectBody = JSON.parse(body);
+        token = objectBody.access_token;
+        refreshToken = objectBody.refresh_token;
+
+        // res.end(body);
+        res.status(200).send(objectBody)
       } else {
         res.status(response.statusCode).end();
         console.log('error = ' + response.statusCode);
       }
     });
   });
+
+//회원 프로필 조회 api
+naverLoginRouter.get('/member', function (req, res) {
+  var api_url = 'https://openapi.naver.com/v1/nid/me';
+  var request = require('request');
+  var options = {
+      url: api_url,
+      headers: {'Authorization': header}
+    };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+
+      // body는 객체타입이 아니라 String이기 때문에 사용하기 쉽도록 객체타입으로 형변환
+      let objectBody = JSON.parse(body);
+
+      // res.end(body);
+      res.status(200).send(objectBody.response)
+    } else {
+      console.log('error');
+      if(response != null) {
+        res.status(response.statusCode).end();
+        console.log('error = ' + response.statusCode);
+      }
+    }
+  });
+});
 
 export { naverLoginRouter }
