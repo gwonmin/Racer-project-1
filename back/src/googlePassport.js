8f7c873e1416b58eede6101b1b5ef24e03d1f113
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-03-24 14:31:14 +0900

    google passport를 활용한  authentication

---
+++ back/src/googlePassport.js
@@ -0,0 +1,29 @@
+
+// passport 설치하기
+// $ npm install passport-google-oauth20
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+// Authenticate Requests
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
